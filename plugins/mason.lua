7814613ee75396eea3b15ea6d57c6c73fe9f3020
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2023-07-18 00:59:23 -0400

    Add all LSP, Linter and Formatter that I always use

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,10 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "tsserver",
+        "eslint",
+        "cssls",
       })
     end,
   },
@@ -18,8 +21,14 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "ts-standard",
+        "eslint_d",
+        "jsonlint",
+        "markdowlint",
+        "quick-lint-js",
+        "fixjson",
+        "prettierd",
+        "standardjs",
       })
     end,
   },
@@ -29,8 +38,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "chrome",
       })
+      opts.automatic_installation = true
     end,
   },
 }
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,17 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "lua",
+      "typescript",
+      "regex",
+      "markdown",
+      "json",
+      "javascript",
+      "bash",
+      "css",
+      "gitignore",
+      "graphql",
+      "html",
     })
   end,
 }

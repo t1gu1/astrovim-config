54c5f1979d357782ca670cc0b89648de24b623c2
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2023-07-29 22:15:53 -0400

    Add an easy way to change the home header with some added headers

--- init.lua
+++ init.lua
@@ -23,7 +23,6 @@ return {
     {
       "catppuccin/nvim",
       name = "catppuccin",
-      config = function() require("catppuccin").setup {} end,
     },
   },
 
--- plugins/core.lua
+++ plugins/core.lua
@@ -3,27 +3,69 @@ return {
   {
     "goolord/alpha-nvim",
     opts = function(_, opts)
-      -- customize the dashboard header
-      opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
+      -- luacheck: push ignore
+      local diablo_skull = {
+        "9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP",
+        " 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP",
+        "  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'",
+        "    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'",
+        "        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~",
+        "                        )b.  .dbo.dP'`v'`9b.odb.  .dX(",
+        "                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.",
+        "                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb",
+        "                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb",
+        "                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP",
+        "                     `'      9XXXXXX(   )XXXXXXP      `'",
+        "                              XXXX X.`v'.X XXXX",
+        "                              XP^X'`b   d'`X^XX",
+        "                              X. 9  `   '  P )X",
       }
+
+      local cutty_cat = {
+        "  ⟋|､",
+        " (°､ ｡ 7",
+        " |､  ~ヽ",
+        " じしf_,)〳",
+      }
+
+      local dead_cutty_cat = {
+        '          !:~~~ .:!M"T#$$$$WX??#MRRMMM!',
+        '          ~?WuxiW*`   `"#$$$$8!!!!??!!!',
+        '        :X- M$$$$       `"T#$T~!8$WUXU~',
+        "       :%`  ~#$$$m:        ~!~ ?$$$$$$",
+        '     :!`.-   ~T$$$$8xx.  .xWW- ~""##*"',
+        "   -~~:<` !    ~?T#$$@@W@*?$$      /`",
+        '!!! .!~~ !!     .:XUW$W!~ `"~:    :',
+        ".:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`",
+        '!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~',
+        ' :X@!.-~   ?@WTWo("*$$$W$TH$! `    ⟋|､',
+        'X$?!-~    : ?$$$B$Wu("**$RM!      (°､ ｡ 7',
+        "~~ !     :   ~$$$$$B$$en:``       |､  ~ヽ",
+        "W.~    :       ~##*$$$M~          じしf_,)〳",
+      }
+
+      local pikachu = {
+        "          ▀████▀▄▄              ▄█ ",
+        "            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ",
+        "    ▄        █          ▀▀▀▀▄  ▄▀  ",
+        "   ▄▀ ▀▄      ▀▄              ▀▄▀  ",
+        "  ▄▀    █     █▀   ▄█▀▄      ▄█    ",
+        "  ▀▄     ▀▄  █     ▀██▀     ██▄█   ",
+        "   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ",
+        "    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ",
+        "   █   █  █      ▄▄           ▄▀   ",
+      }
+      -- luacheck: pop
+
+      -- You can choose between diablo_skull, cutty_cat, dead_cutty_cat, pikachu
+      opts.section.header.val = cutty_cat
 
       local button = require("astronvim.utils").alpha_button
       opts.section.buttons.val = {
         button("LDR S l", "  Last Session  "),
-        button("LDR f o", "󰈙  Recents  "),
+        button("LDR S f", "󰈙  Find Recent sessions"),
         button("LDR f p", "󰈙  Open Project  "),
-        button("LDR n", "  New File"),
+        button("LDR n  ", "  New File   "),
         button("LDR f f", "  Find File  "),
         button("LDR f w", "󰈭  Find Word  "),
         button("LDR f '", "  Bookmarks  "),

dd92383057fe7116a6645c09628ca67575d59778
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2023-08-11 01:40:39 -0400

    Disable lsp formatting of tsserver to only use the project config

--- init.lua
+++ init.lua
@@ -42,6 +42,7 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
+        "tsserver",
       },
       timeout_ms = 1500, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function

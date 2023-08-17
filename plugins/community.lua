19ccfa8403bb5d71a36b87bf20e4eb8b55cb3192
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2023-08-17 16:20:22 -0400

    Changing community plugin order seems to fix some lsp error over time
    Not sure about that

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,17 +7,17 @@ require("astronvim.utils").set_mappings(maps)
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
-  { import = "astrocommunity.editing-support/rainbow-delimiters-nvim" },
   { import = "astrocommunity.project/project-nvim" },
   { import = "astrocommunity.motion/harpoon" },
-  { import = "astrocommunity.utility/noice-nvim" },
-  { import = "astrocommunity.git/git-blame-nvim" },
-  { import = "astrocommunity.lsp/lsp-signature-nvim" },
   { import = "astrocommunity.media/presence-nvim" },
   { import = "astrocommunity.project/nvim-spectre" },
   { import = "astrocommunity.editing-support/todo-comments-nvim" },
   { import = "astrocommunity.motion/nvim-surround" },
   { import = "astrocommunity.pack/godot" },
+  { import = "astrocommunity.git/git-blame-nvim" },
+  { import = "astrocommunity.editing-support/rainbow-delimiters-nvim" },
+  { import = "astrocommunity.utility/noice-nvim" },
+  { import = "astrocommunity.lsp/lsp-signature-nvim" },
 
   -- Customise opts of some community / core plugins
   {

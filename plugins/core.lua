039cbc801d5dde6268b0edb06f029971c8111cbe
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2023-10-11 03:15:40 -0400

    Auto close neo-tree and clean buffer time to time when more than 6 open

--- plugins/core.lua
+++ plugins/core.lua
@@ -23,10 +23,38 @@ return {
     end,
   },
   -- File tree (Move to the right)
+  -- Since I use Harpoon and hide tabs, I want neo-tree to auto manage buffers to close them
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       window = { position = "right", width = 43 },
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function(_file_path)
+            -- auto close buffers when more than 9 has been open and you play with neo tree
+            -- This is kind of an auto cleanup to keep nvim light (DELETE ALL EVENT_HANDLERS IF YOU USE TABS)
+
+            local function tablelength(T)
+              local count = 0
+              for _ in pairs(T) do
+                count = count + 1
+              end
+              return count
+            end
+
+            local get_ls = vim.tbl_filter(
+              function(buf) return vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_get_option(buf, "buflisted") end,
+              vim.api.nvim_list_bufs()
+            )
+
+            if tablelength(get_ls) > 6 then require("astronvim.utils.buffer").close_all(true) end
+            print(vim.inspect(get_ls))
+
+            require("neo-tree.command").execute { action = "close" }
+          end,
+        },
+      },
     },
   },
 }

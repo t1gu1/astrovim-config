7a504d39e6b1cce0738041bb6b7823e7c1a0d7f4
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2023-10-19 23:18:44 -0400

    Add lualine

--- plugins/community.lua
+++ plugins/community.lua
@@ -19,6 +19,52 @@ return {
   { import = "astrocommunity.utility/noice-nvim" },
   { import = "astrocommunity.lsp/lsp-signature-nvim" },
   { import = "astrocommunity.file-explorer/oil-nvim" },
+  { import = "astrocommunity.bars-and-lines/lualine-nvim" },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.sections = {
+        lualine_c = {
+          {
+            "filename",
+            path = 1,
+
+            file_status = true, -- Displays file status (readonly status, modified status)
+            newfile_status = false, -- Display new file status (new file means no write after created)
+
+            shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+            -- for other components. (terrible name, any suggestions?)
+            symbols = {
+              modified = " ●", -- Text to show when the file is modified.
+              readonly = "[Read Only]", -- Text to show when the file is non-modifiable or readonly.
+              unnamed = "[No Name]", -- Text to show for unnamed buffers.
+              newfile = "[New]", -- Text to show for newly created file before first write
+              directory = "",
+            },
+
+            filetype_names = {
+              TelescopePrompt = "Telescope",
+              dashboard = "Dashboard",
+              packer = "Packer",
+              fzf = "FZF",
+              alpha = "Alpha",
+            },
+          },
+        },
+
+        lualine_y = {
+          { "progress" },
+          {
+            "searchcount",
+            maxcount = 999,
+            timeout = 500,
+          },
+        },
+      }
+      return opts
+    end,
+  },
 
   {
     "oil.nvim",
